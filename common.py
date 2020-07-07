574d748fd1364c1006d8f02373f0e42fe9941d0a
Gary Milam Jr <gmilamjr@gmail.com>
2020-07-06 20:13:07 -0400

    Mousewheel control for line size (holding CTRL down) and dilution (using ALT).

--- common.py
+++ common.py
@@ -21,7 +21,8 @@ class Sketcher:
         self.dests = dests
         self.color = color
         self.show()
-        cv2.createTrackbar('Line size', self.windowname, self.size, 20, self.on_sizetrackbar)
+        self.sizetrackname = 'Line size'
+        cv2.createTrackbar(self.sizetrackname, self.windowname, self.size, 20, self.on_sizetrackbar)
         cv2.setMouseCallback(self.windowname, self.on_mouse)
  
     def show(self):
@@ -30,7 +31,17 @@ class Sketcher:
  
     def on_mouse(self, event, x, y, flags, param):
         pt = (x, y)
-        if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN:
+        
+        if event == cv2.EVENT_MOUSEWHEEL:
+            if flags & cv2.EVENT_FLAG_CTRLKEY:
+                if flags > 0:
+                    self.size += 1
+                else:
+                    self.size -= 1
+                cv2.setTrackbarPos(self.sizetrackname, self.windowname, self.size)
+            else:
+                self.mousewheel_events(flags)                
+        elif event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN:
             self.prev_pt = pt
         elif event == cv2.EVENT_LBUTTONUP or event == cv2.EVENT_RBUTTONUP:
             self.prev_pt = None
@@ -50,12 +61,17 @@ class Sketcher:
     def on_sizetrackbar(self, val):
         self.size = val
         
+    def mousewheel_events(self, flags):
+        #Placeholder for mouse wheel events that exist on other classes
+        return
+        
 class DilatedSketcher(Sketcher):
     def __init__(self, windowname, dests):
         super().__init__(windowname, dests, color = 128)
         self.dests[0] = maskOverlay(self.dests[2], self.dests[0])
         self.iter_val = 15
-        cv2.createTrackbar('Dilated Iterations', self.windowname, self.iter_val, 30, self.on_diltrackbar)
+        self.diltrackname = 'Dilated Iterations'
+        cv2.createTrackbar(self.diltrackname, self.windowname, self.iter_val, 30, self.on_diltrackbar)
         self.show()
     
     def show(self):
@@ -72,3 +88,13 @@ class DilatedSketcher(Sketcher):
 
         self.iter_val = val
         self.show()
+    
+    def mousewheel_events(self, flags):
+        if flags & cv2.EVENT_FLAG_ALTKEY:     
+            if flags > 0:
+                self.dests[1] = cv2.dilate(self.dests[1], None, iterations=1)
+                self.iter_val += 1
+            else:
+                self.dests[1] = cv2.erode(self.dests[1], None, iterations=1)
+                self.iter_val -= 1
+            cv2.setTrackbarPos(self.diltrackname, self.windowname, self.iter_val)
